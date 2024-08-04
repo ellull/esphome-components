422fc88dbc0ee45f8979afc35c08023594fbe742
Eduard Llull <eduard@llull.net>
2024-08-04 15:05:18 +0200

    number reboots ESPHome if limits change. This is a workarround of Home Assistant not refreshing the min and max if they change.

--- components/jiecang_desk/number/jiecang_desk_number.h
+++ components/jiecang_desk/number/jiecang_desk_number.h
@@ -14,11 +14,16 @@ class JiecangDeskNumber : public number::Number, public Component, public Jiecan
   void set_parent(JiecangDeskComponent *parent) { this->parent_ = parent; }
   void on_height_update(const optional<int> height) override;
   void on_limits_update(const std::tuple<optional<int>, optional<int>> limits) override;
-   
+  void set_reboot_on_limits_change(const bool value) { this->reboot_on_limits_change_ = value; }
+
  protected:
   void control(const float value) override;
 
   JiecangDeskComponent *parent_{nullptr};
+
+ private:
+  bool configured_{false};
+  bool reboot_on_limits_change_{false};
 };
 
 }  // namespace jiecang_desk
